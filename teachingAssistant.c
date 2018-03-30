18750a1108206f931b9bb5461376fe680ad3bdd3
akhil100on100 <37038436+akhil100on100@users.noreply.github.com>
2018-03-30 18:06:49 +0530

    created two extra functions for minimal coding.

--- teachingAssistant.c
+++ teachingAssistant.c
@@ -1,5 +1,6 @@
 #include<stdio.h>           //input output
-#include<stdlib.h>
+#include<stdlib.h>          //standard library
+#include<string.h>          //string library
 #include<pthread.h>         //create POSIX threads
 #include<semaphore.h>       //creation of semaphores
 
@@ -16,10 +17,8 @@ sem_t s_Tass;
 //mutex
 pthread_mutex_t mutex;
 
-int main(int argc,char *argv[])
-{
-        //work in progress
-}
+int wait_room[3],stud_wait=0,next_seat_pos=0,next_teach_pos=0,ta_flag=0;
+
 void *student(void *stud_id)
 {
         //work in progress
@@ -27,4 +26,50 @@ void *student(void *stud_id)
 void *assistant()
 {
        //work in progress
+}
+int isnum(char num[])
+{
+        int i;
+        for(i=0;num[i]!=0;i++)
+        {
+                if(!isdigit(num[i])
+                {
+                        return 0;
+                }
+        }
+        return 1;
+}
+int iswait(int stud_id)
+{
+        int i;
+        for(i=0;i<3;i++)
+        {
+                if(wait_room[i]==stud_id)
+                {
+                        return 1;
+                }
+        }
+        return 0;
+}
+
+int main(int argc,char *argv[])
+{
+        int i,snum;
+        if(argc>1)
+        {
+                if(isnum(argv[1]==1)
+                {
+                        snum=atoi(argv[1]);
+                }
+                else
+                {
+                        printf("\n*!*!*!*!*!*!*!*INVALID INPUT.....BYE BYE*!*!*!*!*!*!*!*\n");
+                        return 0;
+                }
+        }
+        else
+        {
+                snum=default_students;
+        }
+        //work in progress
 }
