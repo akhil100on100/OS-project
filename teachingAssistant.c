50ac233ef5b470ca86ee55bf370a185a9caf839b
akhil100on100 <37038436+akhil100on100@users.noreply.github.com>
2018-04-09 18:01:57 +0530

    completion of remaining work

--- teachingAssistant.c
+++ teachingAssistant.c
@@ -30,7 +30,7 @@ int isnum(char num[])
         }
         return 1;
 }
-int iswait(int stud_id)
+/*int iswait(int stud_id)
 {
         int i;
         for(i=0;i<3;i++)
@@ -41,12 +41,42 @@ int iswait(int stud_id)
                 }
         }
         return 0;
-}
+}*/
 
 
 void *student(void *stud_id)
 {
-        //work in progress
+        int id=*(int*)stud_id;
+	printf("\nStudent %d is going to TA office",id);
+	while(1)
+	{
+	/*	if(iswait(id)==1)
+		{
+			//printf("\nStudent has to wait....");
+			continue;
+		}*/
+		int p=rand()%5;
+	//	printf("\n\n\tStudent %d is programming for %d seconds...",id,p);
+		sleep(5);
+		pthread_mutex_lock(&mutex);
+		if(stud_wait<wait_chairs)
+		{
+			wait_room[next_seat_pos]=id;
+			stud_wait++;
+			printf("\n\t\tStudent %d has taken a seat in the waiting room....Number of students waiting : %d",id,stud_wait);
+			printf("\nWaiting students : seat-1 <%d>  ||  seat-2 <%d>  ||  seat-3 <%d>",wait_room[0],wait_room[1],wait_room[2]);
+			next_seat_pos=(next_seat_pos+1)%wait_chairs;
+			pthread_mutex_unlock(&mutex);
+			sem_post(&s_stud);
+			sem_wait(&s_Tass);
+		}
+		else
+		{
+			pthread_mutex_unlock(&mutex);
+			printf("\nStudent %d will try later...",id);
+			//printf("\n\n\n\t\t-+-+-+-+-+-+-+-+-NO SEATS AVAILABLE.....BETTER LUCK NEXT TIME-+-+-+-+-+-+-+-+-");
+		}
+	}
 }
 void *assistant()
 {
@@ -83,22 +113,39 @@ void *assistant()
 
 int main(int argc,char *argv[])
 {
-        int i,snum;
-        if(argc>1)
+    int i,snum;
+    if(argc>1)
+    {
+        if(isnum(argv[1]==1))
         {
-                if(isnum(argv[1]==1)
-                {
-                        snum=atoi(argv[1]);
-                }
-                else
-                {
-                        printf("\n*!*!*!*!*!*!*!*INVALID INPUT.....BYE BYE*!*!*!*!*!*!*!*\n");
-                        return 0;
-                }
+           	snum=atoi(argv[1]);
         }
         else
         {
-                snum=default_students;
+            printf("\n*!*!*!*!*!*!*!*INVALID INPUT.....BYE BYE*!*!*!*!*!*!*!*\n");
+            return 0;
         }
-        //work in progress
+    }
+    else
+    {
+        snum=default_students;
+    }
+    int s_ids[snum];
+    pthread_t st[snum],t;
+    sem_init(&s_stud,0,0);
+    sem_init(&s_Tass,0,1);
+    pthread_mutex_init(&mutex,NULL);
+    pthread_create(&t,NULL,assistant,NULL);
+    for(i=0;i<snum;i++)
+	{
+		s_ids[i]=i+1;
+		pthread_create(&st[i],NULL,student,(void*)&s_ids[i] );
+	}
+	//Join threads
+	pthread_join(t,NULL);
+	for(i=0;i<snum;i++)
+	{
+		pthread_join(st[i],NULL);
+	}
+	return 0;
 }
