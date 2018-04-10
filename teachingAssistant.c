5f09680207895c157c6775fbef4fd50896f026b2
akhil100on100 <37038436+akhil100on100@users.noreply.github.com>
2018-04-10 23:37:21 +0530

    final updation

--- teachingAssistant.c
+++ teachingAssistant.c
@@ -1,63 +1,30 @@
-#include<stdio.h>           //input output
-#include<stdlib.h>          //standard library
-#include<string.h>          //string library
-#include<pthread.h>         //create POSIX threads
-#include<semaphore.h>       //creation of semaphores
+#include <stdio.h>         
+#include <stdlib.h>          
+#include <string.h>         
+#include <pthread.h>        
+#include <semaphore.h>       
+#include <unistd.h>
 
-#define wait_chairs 3             //no. of waiting chairs
-#define default_students 5        //default no. of students
+#define wait_chairs 3           
 
-void *student(void *stud_id);     //Student's activity
-void *assistant();                //teaching assistant's activity
+void *student(void *stud_id);     
+void *assistant();              
 
-//semaphores
 sem_t s_stud;
 sem_t s_Tass;
 
-//mutex
 pthread_mutex_t mutex;
 
 int wait_room[3],stud_wait=0,next_seat_pos=0,next_teach_pos=0,ta_flag=0;
-int isnum(char num[])
-{
-        int i;
-        for(i=0;num[i]!=0;i++)
-        {
-                if(!isdigit(num[i])
-                {
-                        return 0;
-                }
-        }
-        return 1;
-}
-/*int iswait(int stud_id)
-{
-        int i;
-        for(i=0;i<3;i++)
-        {
-                if(wait_room[i]==stud_id)
-                {
-                        return 1;
-                }
-        }
-        return 0;
-}*/
-
 
 void *student(void *stud_id)
 {
-        int id=*(int*)stud_id;
+	int id=*(int*)stud_id;
 	printf("\nStudent %d is going to TA office",id);
 	while(1)
 	{
-	/*	if(iswait(id)==1)
-		{
-			//printf("\nStudent has to wait....");
-			continue;
-		}*/
 		int p=rand()%5;
-	//	printf("\n\n\tStudent %d is programming for %d seconds...",id,p);
-		sleep(5);
+		sleep(p);
 		pthread_mutex_lock(&mutex);
 		if(stud_wait<wait_chairs)
 		{
@@ -74,7 +41,6 @@ void *student(void *stud_id)
 		{
 			pthread_mutex_unlock(&mutex);
 			printf("\nStudent %d will try later...",id);
-			//printf("\n\n\n\t\t-+-+-+-+-+-+-+-+-NO SEATS AVAILABLE.....BETTER LUCK NEXT TIME-+-+-+-+-+-+-+-+-");
 		}
 	}
 }
@@ -83,7 +49,7 @@ void *assistant()
     printf("Checking for students.\n");
     while(1)
     {
-	 	if(stud_wait>0)
+	if(stud_wait>0)
     	{
        		ta_flag=0;
        		sem_wait(&s_stud);
@@ -95,40 +61,29 @@ void *assistant()
 		printf("\nWaiting students : seat-1 <%d>  ||  seat-2 <%d>  ||  seat-3 <%d>",wait_room[0],wait_room[1],wait_room[2]);
 		next_teach_pos=(next_teach_pos+1)%wait_chairs;
 		sleep(ht);
-		printf("\n\t*!*!*!*!*!*!*!*!*!TEACHING FINISH!*!*!*!*!*!*!*!*!*");
-		//printf("\nNumber of students waiting : %d",stud_wait);
+		printf("\n\t*!*!*!*!*!*!*!*!*!TEACHING FINISH!*!*!*!*!*!*!*!*!*\n");
 		pthread_mutex_unlock(&mutex);
-	        sem_post(&s_Tass);
-		}
-		else
+		sem_post(&s_Tass);
+	}
+	else
+	{
+		if(ta_flag==0)
 		{
-			if(ta_flag==0)
-			{
-				printf("\n\n\t#-#-#-#-#-#-#-#-#-NO STUDENTS WAITING....TEACHER IS SLEEPING-#-#-#-#-#-#-#-#-#");
-				ta_flag=1;
-			}
+			printf("\n\n\t#-#-#-#-#-#-#-#-#-NO STUDENTS WAITING....TEACHER IS SLEEPING-#-#-#-#-#-#-#-#-#");
+			ta_flag=1;
 		}
 	}
+    }
 }
-
-int main(int argc,char *argv[])
+int main()
 {
     int i,snum;
-    if(argc>1)
+    printf("\nenter number of students: ");
+    scanf("%d",&snum);
+    if(snum==0)
     {
-        if(isnum(argv[1]==1))
-        {
-           	snum=atoi(argv[1]);
-        }
-        else
-        {
-            printf("\n*!*!*!*!*!*!*!*INVALID INPUT.....BYE BYE*!*!*!*!*!*!*!*\n");
-            return 0;
-        }
-    }
-    else
-    {
-        snum=default_students;
+    	printf("\t\tNO STUDENTS...\n");
+    	return 0;
     }
     int s_ids[snum];
     pthread_t st[snum],t;
@@ -137,15 +92,14 @@ int main(int argc,char *argv[])
     pthread_mutex_init(&mutex,NULL);
     pthread_create(&t,NULL,assistant,NULL);
     for(i=0;i<snum;i++)
-	{
-		s_ids[i]=i+1;
-		pthread_create(&st[i],NULL,student,(void*)&s_ids[i] );
-	}
-	//Join threads
-	pthread_join(t,NULL);
-	for(i=0;i<snum;i++)
-	{
-		pthread_join(st[i],NULL);
-	}
-	return 0;
+    {
+	s_ids[i]=i+1;
+	pthread_create(&st[i],NULL,student,(void*)&s_ids[i] );
+    }
+    pthread_join(t,NULL);
+    for(i=0;i<snum;i++)
+    {
+	pthread_join(st[i],NULL);
+    }
+    return 0;
 }
